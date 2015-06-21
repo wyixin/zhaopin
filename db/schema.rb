881150b049af781798e6c4739b4fb1d18559f0af
wyixin <single.w.1992@gmail.com>
2015-06-21 23:57:42 +0800

    artile & caterory scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150621054519) do
+ActiveRecord::Schema.define(version: 20150621152508) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.text     "text",        limit: 65535
+    t.integer  "category_id", limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "slug",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "categories", ["name"], name: "index_categories_on_name", unique: true, using: :btree
+  add_index "categories", ["slug"], name: "index_categories_on_slug", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
