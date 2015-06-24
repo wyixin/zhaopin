34ba5cd6a6daf995fd2d94e6517795cca377e59a
jiangyao <249640592@qq.com>
2015-06-24 17:34:32 +0800

    init_resume

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -51,7 +51,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    p resource
+    resource.init_resume
     super(resource)
   end
 
