5a33e4be40d88934fc253f937e81697da1d1e74d
jiangyao <249640592@qq.com>
2015-06-25 14:49:33 +0800

    招聘信息

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   def index
-
+    @categories = Category.all
+    @hot_sub_categories = SubCategory.where(:category_id=>6)
   end
 end
--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,16 @@
 module JobsHelper
+
+  def hot_category_class(sub_category_id)
+    case sub_category_id
+      when 7
+        'warm'
+      when 8
+        'info'
+      when 9
+        'success'
+      else
+        'hot'
+    end
+  end
+
 end
