ff29a9c9b7d25718e47e73a426bcd04dd385fa22
jiangyao <249640592@qq.com>
2015-06-27 10:20:19 +0800

    企业管理修改

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -3,7 +3,7 @@ puts "deleting Account ..."
 #User.delete_all
 #ActiveRecord::Base.connection.execute "ALTER TABLE sys_accounts AUTO_INCREMENT = 1"
 
-puts "Creating admin user as admin/11111111 ..."
+puts "Creating admin user as admin@123.com/11111111 ..."
 User.create(email: 'admin@123.com',
                password: '11111111',
                password_confirmation: '11111111',
